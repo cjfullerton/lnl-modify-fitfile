ce40fd1c400e57f438dbd9bcb94e38e03aa75883
chrisf <3920981+cjfullerton@users.noreply.github.com>
2021-02-25 14:40:02 +0000

    bug fix

--- lnl-modify-fitfile_OSX.sh
+++ lnl-modify-fitfile_OSX.sh
@@ -3,7 +3,7 @@ echo "-> start"
 for folder in `cat $1`;
 do
     sed -i '' -e "2s/.*/fitprofile=\"$2\"/" $folder/fitfile.txt
-    $echo $folder/fitfile.txt modified...
+    echo $folder/fitfile.txt uses model profile $2
 done
 echo "-> finished"
 exit 
